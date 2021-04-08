0a193e606055b3dc1375072f7d0ae268f23fdab1
ch-canaza <ch-canaza@hotmail.com>
2021-04-08 14:58:18 -0500

    drying

--- spec/controllers/devise_controller_spec.rb
+++ spec/controllers/devise_controller_spec.rb
@@ -2,9 +2,11 @@
 
 require 'rails_helper'
 require_relative './support/devise'
+require 'capybara/rspec'
 
 RSpec.describe DeviseController, type: :controller do
   describe 'GET /' do
+    include Capybara::DSL
     login_user
 
     context 'when login user' do
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email) { |n| "test-#{n.to_s.rjust(3, "0")}@sample.com" }
+    sequence(:email) { |n| "test-#{n.to_s.rjust(3, "0")}@sample2.com" }
     password { "123456" }
   end
+  
 end
